964d28a4027669afb966182a6961b814d6df77c7
twinh <twinhuang@qq.com>
2019-02-08 15:44:24 +0800

    refactor: modal更新

--- resources/views/link-to/link-to.php
+++ resources/views/link-to/link-to.php
@@ -13,8 +13,8 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
           <h4 class="modal-title">设置链接</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
         </div>
         <div class="modal-body">
           <div class="form-group">
