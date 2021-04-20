6a6f51b785819915c8d5e09d6fd2c3cc8a6471c9
twinh <twinhuang@qq.com>
2021-04-20 15:40:36 +0800

    feat: 增加 `LinkPicker` 组件，通过事件获取链接选项

---
+++ components/LinkPicker.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from "react";
+import {default as BaseLinkPicker} from '@mxjs/a-link-picker';
+import {event} from '@mxjs/app';
+
+const LinkPicker = (props) => {
+  const [options, setOptions] = useState([]);
+
+  // 加载选项
+  useEffect(() => {
+    (async () => {
+      let options = [];
+      await event.trigger('linkPickerGetOptions', [options]);
+      setOptions(options);
+    })();
+  }, []);
+
+  return (
+    <BaseLinkPicker options={options} {...props}/>
+  );
+};
+
+export default LinkPicker;
